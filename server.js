ca8564b38d180fec8e8e13326148175725a799fe
Cynthia Deng <cynthiadeng@Cynthia.local>
2024-09-24 08:34:06 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+/*********************************************************************************
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: Shuyu Deng
+Student ID: 117587238
+Date: September 26
+Vercel Web App URL: https://helloworld-iota-eight.vercel.app/
+GitHub Repository URL: https://github.com/shuyu2024/helloworld.git
+
+********************************************************************************/ 
